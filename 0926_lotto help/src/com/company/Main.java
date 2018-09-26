a9d3e5b36df386f9ce63ee8b0f8a23b80956534c
J. Evan Noynaert <noynaert@missouriwestern.edu>
2018-09-26 18:24:32 -0500

    another start on homework 3

--- 0926_lotto help/src/com/company/Main.java
+++ 0926_lotto help/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -8,8 +10,8 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input;
-        LocalDate startDate
-        LocalDate endDate
+        //LocalDate startDate
+        //LocalDate endDate
 
         /* Stuff we already did in class, I think
         int totalDataLines;
@@ -28,6 +30,30 @@ public class Main {
         System.out.println(" Program by your name");
     }
     public static int processFile(LocalDate startDate, LocalDate enddate, int[] counts){
+        int n = 0;
 
+        try {
+            Scanner input = new Scanner(new File("lotto.txt"));
+            while(input.hasNextLine()){
+                String line;
+                String pattern = "(Sat)|(Wed),.*Million";
+                line = input.nextLine();
+                if(line.matches(pattern)) {
+                    System.out.println("Line: " + line);  //comment out this line
+                    String[] parts = line.split("\\s+");
+                    System.out.println("The year string is "+ parts[3] );
+                    n++;
+                }
+
+            }
+
+
+            input.close();
+        } catch (FileNotFoundException e) {
+            //e.printStackTrace();
+            System.err.println("Could not find file lotto.txt");
+            System.exit(1);
+        }
+        return n;
     }
 }
